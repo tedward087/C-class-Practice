ee70b3a115258fbf17d3870cbdeaf505ab8264a4
tedward087 <tedward087@gmail.com>
2021-07-22 00:42:00 +0800

    7.21

---
+++ Array 練習/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    /*
+    //練習2
+    string str_Mag="Hello";
+    string str_Mag2="H";
+    cout<< sizeof(str_Mag) <<"," <<sizeof(str_Mag2)<<"\n";
+
+    cout<< sizeof(str_Mag[0]);
+    */
+
+    /*
+    //老師出的練習
+    int ia_3D[3][2][1]={{{1},{2}},{{3},{4}},{{5},{6}}};
+    cout<< ia_3D [1][1][0];
+    int* ip_3DArr;
+    ip_3DArr = reinterpret_cast<int*>(ia_3D);
+    cout<<endl<<*(ip_3DArr+1*2+1*1+0*0);
+    */
+
+    /*
+    //練習3
+    char ca_Id[]={'A','1','2','3','4','5','6','7','8','9','\0',};
+    for(int i_a=0;i_a<10;i_a++){
+        cout<<ca_Id[i_a]<<endl;
+    }
+    */
+
+    /*
+    //練習4
+    string ca_Id = "A123456789";
+    for(int i_A=0;i_A<10;i_A++){
+        cout<<ca_Id[i_A]<<endl;
+    }
+    */
+    return 0;
+}
