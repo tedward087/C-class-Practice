b112572698387626e4ed60885ae767d4569295df
tedward087 <tedward087@gmail.com>
2021-07-31 23:49:58 +0800

    7.31

---
+++ Topic 11/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	/*
+	//°}¦C ½m²ß3 ¥Î¦V¶q¼g
+    vector<char> v_Id;
+    v_Id.push_back('A');
+    v_Id.push_back('1');
+    v_Id.push_back('2');
+    v_Id.push_back('3');
+    v_Id.push_back('4');
+    v_Id.push_back('5');
+    v_Id.push_back('6');
+    v_Id.push_back('7');
+    v_Id.push_back('8');
+    v_Id.push_back('9');
+    for(vector<char>::iterator o_Id=v_Id.begin();o_Id!=v_Id.end();o_Id++){
+
+		cout<<*o_Id<<"\n";
+    }
+
+
+    */
+    return 0;
+}
--- 遞迴/main.cpp
+++ 遞迴/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 /*
-//�m��1(�Ѯv�g��
+//練習1(老師寫的)
 int fn_GetHCF(int i_Num1, int i_Num2);
 
 int main(){
     int i_Num1 =0;
     int i_Num2 =0;
-    std::cin>>i_Num1 >> i_Num2;
+    cout<<"Please enter two integer numbers: ";
+    cin>>i_Num1 >> i_Num2;
 
-    std::cout<< fn_GetHCF(i_Num1, i_Num2);
+    cout<<"The HCF is "<< fn_GetHCF(i_Num1, i_Num2);
     return 0;
 }
 
@@ -32,4 +35,78 @@ int fn_GetHCF(int i_Num1, int i_Num2){
     }
 }
 */
+
+/*
+//最小公倍數
+int fn_GetLCM(int i_Num1, int i_Num2);
+
+int main(){
+    int i_Num1 =0;
+    int i_Num2 =0;
+    int i_LCM =1;
+    cin>>i_Num1 >> i_Num2;
+
+
+    i_LCM=fn_GetLCM(i_Num1, i_Num2);
+    cout<<i_LCM*(i_Num1/i_LCM)*(i_Num2/i_LCM);
+    return 0;
+}
+
+int fn_GetLCM(int i_Num1, int i_Num2){
+    int i_Max=0;
+    int i_Min=0;
+    if(i_Num1 <= i_Num2){
+        i_Min = i_Num1;
+        i_Max = i_Num2;
+    }
+    else{
+        i_Min = i_Num2;
+        i_Max = i_Num1;
+    }
+    if(i_Min == 0){
+        return i_Max;
+    }
+    else{
+        return fn_GetLCM(i_Max% i_Min, i_Min);
+    }
+}
+
+*/
+
+
+//老師出的題 3
+//我寫的(沒用迴圈)
+int fn_money(int z);
+
+int main(){
+int z=0;
+cin>>z;
+cout<<fn_money(z);
+
+return 0;
+}
+
+
+int fn_money(int z){
+int b1=11;
+int fa[z]={};
+for(int i=0;i<z;i++){
+	if(i==0)
+		{fa[i]=1;}
+	else if(i==1)
+		{fa[i]=1;}
+	else
+		{fa[i]=fa[i-1]+fa[i-2];}
+
+
+}
+ if(z%2==1){
+	return (fa[z-2])*4+11*fa[z-1];
+}
+else if(z%2==0){
+	return (fa[z-2])*7+11*fa[z-1];
+}
+
+
+}
 
